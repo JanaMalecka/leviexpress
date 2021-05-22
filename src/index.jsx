7d08f3d7d9323638720899468fe724e254a736f9
Jana Malecka <jm.jmalecka@gmail.com>
2021-05-22 10:34:18 +0200

    Header a Footer hotove

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
-const App = () => <div className="container"></div>;
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
 
 render(<App />, document.querySelector('#app'));
