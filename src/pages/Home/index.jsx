1752acf325ac0af159c11ca0cd4ff83da420892d
Jana Malecka <jm.jmalecka@gmail.com>
2021-05-22 15:52:08 +0200

    podrobnosti cesty se zobrazuji

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
 import './style.css';
 import { JourneyPicker } from './JourneyPicker';
+import { JourneyDetail } from '../JourneyDetail';
 
 export const Home = () => {
   const [journey, setJourney] = useState(null);
   const handleJourneyChange = (props) => {
     setJourney(props);
     /* Propojte komponentu JourneyPicker se stavem journey – když komponenta JourneyPicker zavolá onJourneyChange s údaji o nalezeném spoji, nastaví se toto spojení do stavu journey. */
-    console.log(props);
+    /* console.log(props); */
   };
   return (
     <>
       <h2>Doma</h2>
       {console.log(journey)}
       <JourneyPicker onJourneyChange={handleJourneyChange} />
-      {journey ? <p>Nalezeno spojeni s id {journey.journeyId}</p> : null}
+      {/*  {journey ? <p>Nalezeno spojeni s id {journey.journeyId}</p> : null} */}
+      {journey ? <JourneyDetail journey={journey} /> : null}
     </>
   );
 };
